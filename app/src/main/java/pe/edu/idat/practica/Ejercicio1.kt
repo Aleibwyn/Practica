f23392975861600791a90b57188369bc3c59c1be
Aleibwyn <mf-a@outlook.com>
2024-02-22 16:38:58 -0500

    zzzzzzzz

---
+++ app/src/main/java/pe/edu/idat/practica/Ejercicio1.kt
@@ -0,0 +1,11 @@
+package pe.edu.idat.practica
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Ejercicio1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ejercicio1)
+    }
+}
