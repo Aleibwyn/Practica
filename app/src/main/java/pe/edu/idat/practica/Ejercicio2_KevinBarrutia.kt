13980b3ca4e4261cff96ab061aeb850771f9d466
Garou1207 <kevinbarrutia@outlook.com>
2024-02-22 17:00:13 -0500

    Ejercicio02 Kevin Barrutia

---
+++ app/src/main/java/pe/edu/idat/practica/Ejercicio2_KevinBarrutia.kt
@@ -0,0 +1,11 @@
+package pe.edu.idat.practica
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Ejercicio2_KevinBarrutia : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ejercicio2_kevin_barrutia)
+    }
+}
